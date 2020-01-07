c619f3d573ec999ed6952f709dd9c304ddd697e8
33504 <feng.bie@myt11.com>
2020-01-07 15:41:11 +0800

    common commit

--- src/main/java/com/fh/controller/TestController.java
+++ src/main/java/com/fh/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.fh.controller;
 
+import com.fh.service.ProjectFileService;
 import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,38 +14,42 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/projectFile")
 public class TestController {
 
-        // 处理文件上传
-        @PostMapping("/upload")
+    @Autowired
+    private ProjectFileService projectFileService;
 
-        public String projectFileUpload(HttpServletRequest request,
-                                        @RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
 
-            if(!file.isEmpty()) {
-                //上传文件路径
-                String path = request.getServletContext().getRealPath("/images/");
-                //上传文件名
-                String filename = file.getOriginalFilename();
-                File filepath = new File(path,filename);
-                //判断路径是否存在，如果不存在就创建一个
-                if (!filepath.getParentFile().exists()) {
-                    filepath.getParentFile().mkdirs();
-                }
-                //将上传文件保存到一个目标文件当中
-                file.transferTo(new File(path + File.separator + filename));
-                return "success";
-            } else {
-                return "error";
+    // 处理文件上传
+    @PostMapping("/upload")
+    public String projectFileUpload(HttpServletRequest request,
+                                    @RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
+
+        if (!file.isEmpty()) {
+            //上传文件路径
+            String path = request.getServletContext().getRealPath("/images/");
+            //上传文件名
+            String filename = file.getOriginalFilename();
+            File filepath = new File(path, filename);
+            //判断路径是否存在，如果不存在就创建一个
+            if (!filepath.getParentFile().exists()) {
+                filepath.getParentFile().mkdirs();
             }
-
+            //将上传文件保存到一个目标文件当中
+            file.transferTo(new File(path + File.separator + filename));
+            return "success";
+        } else {
+            return "error";
         }
 
+    }
 
-        // 设置上下方文
+
+    // 设置上下方文
 /*        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
                 request.getSession().getServletContext());
 
@@ -72,32 +78,32 @@ public class TestController {
     }
 
 */
-        @GetMapping("/download")
-        public ResponseEntity<byte[]> download(HttpServletRequest request,
-                                               @RequestParam("filename") String filename,
-                                               Model model)throws Exception {
-            //下载文件路径
-            String path = request.getServletContext().getRealPath("/images/");
-            File file = new File(path + File.separator + filename);
-            HttpHeaders headers = new HttpHeaders();
-            //下载显示的文件名，解决中文名称乱码问题
-            String downloadFileName = new String(filename.getBytes("UTF-8"),"iso-8859-1");
-            //通知浏览器以attachment（下载方式）打开图片
-            headers.setContentDispositionFormData("attachment", downloadFileName);
-            //application/octet-stream ： 二进制流数据（最常见的文件下载）。
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),
-                    headers, HttpStatus.CREATED);
-        }
-
-
-
-
-        @GetMapping("/test")
-        public String test() {
-            return "success";
-        }
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> download(HttpServletRequest request,
+                                           @RequestParam("filename") String filename,
+                                           Model model) throws Exception {
+        //下载文件路径
+        String path = request.getServletContext().getRealPath("/images/");
+        File file = new File(path + File.separator + filename);
+        HttpHeaders headers = new HttpHeaders();
+        //下载显示的文件名，解决中文名称乱码问题
+        String downloadFileName = new String(filename.getBytes("UTF-8"), "iso-8859-1");
+        //通知浏览器以attachment（下载方式）打开图片
+        headers.setContentDispositionFormData("attachment", downloadFileName);
+        //application/octet-stream ： 二进制流数据（最常见的文件下载）。
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),
+                headers, HttpStatus.CREATED);
     }
+
+
+    @GetMapping("/test")
+    public String test() {
+        Map test = projectFileService.test(null, null);
+        System.out.println(test);
+        return "success";
+    }
+}
 /*    public Map<String, String> uploadFile(MultipartFile txt) throws IOException {
         Map<String, String> result = new HashMap<>();
         try (FileOutputStream fos = new FileOutputStream("./test.txt")) {
